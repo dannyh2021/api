f51c4f5435aa435a02cfd4c6c6407560bf563604
Danny Huang <32226840+dannyh2021@users.noreply.github.com>
2024-02-29 13:34:40 -0500

    updated index.js, middle of chapter 4

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// GraphQL setup
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World!'
+    }
+};
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.get('/', (req, res) => res.send('Hello Web Server!!!'));
+
+app.listen({ port }, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}!`));

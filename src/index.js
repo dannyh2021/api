23735530565b15f02213f706eb4eb4e058afdb30
Danny Huang <32226840+dannyh2021@users.noreply.github.com>
2024-02-29 14:08:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,56 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
 const port = process.env.PORT || 4000;
+
+// sample data
+let notes = [
+    { id: '1', content: 'This is a note', author: 'Adam Scott' },
+    { id: '2', content: 'This is a another note', author: 'Harlow Everly' },
+    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
 
 // GraphQL setup
 const typeDefs = gql`
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+
     type Query {
         hello: String
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+
+    type Mutation {
+        newNote(content: String!): Note!
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => 'Hello World!'
+        hello: () => 'Hello World!',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Adam Scott'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
+
+const app = express();
 
 // Apollo Server setup
 const server = new ApolloServer({ typeDefs, resolvers });
